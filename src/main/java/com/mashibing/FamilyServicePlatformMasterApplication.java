96da050d319b33f5b1abd51d6df5a8e64666c9eb
liuxb <liuxb@eptok.com>
2020-09-08 14:43:02 +0800

    Initial commit

---
+++ src/main/java/com/mashibing/FamilyServicePlatformMasterApplication.java
@@ -0,0 +1,13 @@
+package com.mashibing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyServicePlatformMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyServicePlatformMasterApplication.class, args);
+    }
+
+}
